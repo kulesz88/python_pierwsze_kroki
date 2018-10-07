d90ed446110f63051807264a98f6b29e5529190e
kulesz88 <kulesz88@users.noreply.github.com>
2018-10-07 14:29:02 +0200

    Funkcja

--- main.py
+++ main.py
@@ -19,14 +19,41 @@
 #print(imie + ' ma psa o imieniu ' + imie_psa + ' ktory wazy ' + str(waga_psa))
 #print("Kolor psa to " + kolor_psa)
 
-rowery = ['romet', 'unibike', 'accent']
-ilosc = [5, 6, 14]
+#rowery = ['romet', 'unibike', 'accent']
+#ilosc = [5, 6, 14]
 
 #for il in ilosc:
 #    print(il)
 
-print("Dlugosc: {0}".format(len(rowery)))
+#print("Dlugosc: {0}".format(len(rowery)))
 
-for idx in range(len(rowery) ) :
-    print( "idx: " + str(idx) + ": " + rowery[idx])
-    print(rowery[idx] + " jest na stanie " + str(ilosc[idx]) + " sztuk")
+#for idx in range(len(rowery) ) :
+#    print( "idx: " + str(idx) + ": " + rowery[idx])
+#    print(rowery[idx] + " jest na stanie " + str(ilosc[idx]) + " sztuk")
+
+#rower = {'marka': "romet",
+#         'model': "gazela",
+#         'waga': 10}
+#
+#print(rower['marka'])
+#
+#for key, value in rower.iteritems():
+#    print("{0}:{1}".format(key, value))
+#
+#for key in rower:
+#    print("{0}:{1}".format(key, rower[key]))
+
+#Funkcja
+
+def print_dict(d):
+    for key, value in d.iteritems():
+        print("{0}:{1}".format(key, value))
+
+if __name__ == "__main__":
+    rower = {'marka': "romet",
+             'model': "gazela",
+             'waga': 10}
+
+print_dict(rower)
+print_dict(rower)
+print_dict(rower)
